a8b5f9bdf7773861b94b2ddc049fbc7e4b335ac0
bermudaa <philipp.harsleben@gmail.com>
2016-08-27 18:09:29 +0200

    Update main.js to fix problem with version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,24 @@
-console.log('HELLO WORLD');
-console.log('Hello again');
+var model = {
+	init: function() {
+		console.log('Model initialized');
+	}
+};
+
+var helper = {
+
+};
+
+var view = {
+	init: function() {
+		console.log('View initialized');
+	}
+};
+
+var app = {
+	init: function() {
+		model.init();
+		view.init();
+	}
+};
+
+app.init();
